1da9237faf5133a5f6eab467ed818e1c671024c8
JoeyChen <452250588@qq.com>
2021-05-19 00:59:57 +0800

    init

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+// import Vue from 'vue'
+// import { Button } from 'element-ui'
+
+// Vue.use(Button)
+
+import { createApp } from 'vue'
+import { Button } from 'element-ui'
+
+createApp().use(Button)
